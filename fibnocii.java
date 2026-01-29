f282ee410da3157241d7d77674054eac8dc496d6
Prachi dhawan <prachi1575.becse24@chitkara.edu.in>
2026-01-30 03:50:55 +0530

    fibnocii sequence

---
+++ fibnocii.java
@@ -0,0 +1,19 @@
+public class fibnocii {
+    
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=1;i<n;i++){
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+    public static int fibonacci(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
