661ff6c23ac06071588d892e3818bb92c4dd27d0
Prachi dhawan <prachi1575.becse24@chitkara.edu.in>
2026-02-17 01:57:27 +0530

    quick sort logic

---
+++ quick_sort.java
@@ -0,0 +1,34 @@
+public class quick_sort {
+    public static int quickSort(int[]arr,int low,int high){
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
+        return i+1;
+    } 
+    public static void merge(int[]arr,int low,int high){
+        if(low<high){
+            int pi=quickSort(arr, low, high);
+            merge(arr, low, pi-1);
+            merge(arr, pi+1, high);
+        }
+    }
+    public static void main(String[] args) {
+        int[]  arr={10,80,30,90,40,50,70};
+        int n=arr.length;
+        merge(arr, 0, n-1);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
